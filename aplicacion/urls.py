89db1331428d0b55d38bec1813090bc49a754bac
Jarriv <brajhan1@hotmail.com>
2020-09-02 16:29:00 -0500

    html actualizado

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', home,name= "index"),
     path('crear_persona/',CreatePersona.as_view(), name= "crear_persona"),
     path('listar_persona/',ListPersona.as_view(), name= "listar_persona"),
-    path('editar_persona/',UpdatePersona.as_view(), name= "editar_persona"),
-    path('eliminar_persona/',DeletePersona.as_view(), name= "eliminar_persona"),
+    path('editar_persona/<int:pk>',UpdatePersona.as_view(), name= "editar_persona"),
+    path('eliminar_persona/<int:pk>',DeletePersona.as_view(), name= "eliminar_persona"),
 ]
