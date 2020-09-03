51486a26b837faf389bd30b3e18a2e0b4fbcef61
Jarriv <brajhan1@hotmail.com>
2020-09-03 16:37:09 -0500

    aplicacion/urls.py: Syntax ERROR '/' Fixed!!

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,11 +1,12 @@
-from django.urls import include, path
+from django.urls import path
 from .views import *
 from .forms import PersonaForm
+from . import views
 
 urlpatterns = [
     path('', home,name= "index"),
     path('crear_persona/',CreatePersona.as_view(), name= "crear_persona"),
     path('listar_persona/',ListPersona.as_view(), name= "listar_persona"),
-    path('editar_persona/<int:pk>',UpdatePersona.as_view(), name= "editar_persona"),
-    path('eliminar_persona/<int:pk>',DeletePersona.as_view(), name= "eliminar_persona"),
+    path('editar_persona/<int:pk>/',UpdatePersona.as_view(), name= "editar_persona"),
+    path('eliminar_persona/<int:pk>/',DeletePersona.as_view(), name= "eliminar_persona"),
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import PersonaForm
-from django.views.generic import CreateView, UpdateView, ListView, DeleteView
+from django.views.generic import CreateView,ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -12,12 +12,12 @@ class CreatePersona(CreateView):
     model = Persona
     form_class = PersonaForm 
     template_name = 'aplicacion/crear_persona.html'
-    success_url = reverse_lazy('listar_persona')
+    success_url = reverse_lazy('listar_persona')    
 
 class ListPersona(ListView):
     model = Persona
     template_name = 'aplicacion/listar_persona.html'
-
+    
 class UpdatePersona(UpdateView):
     model = Persona
     form_class = PersonaForm
